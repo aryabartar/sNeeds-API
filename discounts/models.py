848b9f053ae52918b013d1c07829f8876611a862
Arya Khaligh <a.khaligh@aut.ac.ir>
2018-10-25 08:28:29 +0330

    Request to discount page added .

--- discounts/models.py
+++ discounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 # Create your models here.
+from django.urls import reverse
+
 
 class Cafe(models.Model):
     name = models.CharField(max_length=128, null=True, blank=True)
@@ -9,6 +11,10 @@ class Cafe(models.Model):
     address = models.CharField(max_length=256, blank=False, default="تهران")
     phone_number = models.CharField(max_length=128, blank=True, null=True)
     slug = models.SlugField(max_length=128 , blank=False , unique=True , default="default")
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('discounts:cafe_page', args=[str(self.slug)])
 
     def __str__(self):
         return self.name
