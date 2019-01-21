d4aa3a1468e1cd3bc29b8950b7968d928c7f22f0
arya <a.khaligh@aut.ac.ir>
2019-01-21 14:15:47 +0330

    Add cafe_profile

--- account/urls.py
+++ account/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     # TODO: Change _ with -
     path('my-account/', views.my_account, name="my_account"),
     path('my-account-new/', views.my_account1, name="my_account1"),
+    path('cafe-profile-new/', views.my_account2, name="cafe_profile"),
     path('my-account/discount-archive/', views.all_cafe_archive, name='cafe_discount_archives'),
     path('delete/discount/', views.delete_user_discount, name='delete_discount'),
     path('delete/cafe-discount/', views.delete_cafe_discount, name='delete_cafe_discount'),
