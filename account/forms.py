a1be09837b5a212dfc2aa4a0f86da074c75109a9
arya <a.khaligh@aut.ac.ir>
2019-01-22 16:52:26 +0330

    Modify signup forms.

--- account/forms.py
+++ account/forms.py
@@ -4,17 +4,30 @@ from django import forms
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="آدرس ایمیل", required=True, widget=forms.EmailInput(), max_length=120)
-    phone_number_field = forms.CharField(required=True, label="شماره تماس",
-                                         error_messages={'required': 'لطفا شماره تماس خود را وارد کنید'}, max_length=12)
+    email = forms.EmailField(label="آدرس ایمیل",
+                             required=True,
+                             max_length=120,
+                             widget=forms.EmailInput(
+                                 attrs={'placeholder': 'ایمیل خود را وارد نمایید'}
+                             )
+                             )
+
+    phone_number_field = forms.CharField(required=True,
+                                         label="شماره تماس",
+                                         error_messages={'required': 'لطفا شماره تماس خود را وارد کنید'},
+                                         max_length=12,
+                                         widget=forms.TextInput(
+                                             attrs={'placeholder': 'شماره موبایل خود را وارد نمایید'}
+                                         ),
+                                         )
 
     class Meta:
         model = User
         fields = ('username',
                   'email',
-                  'phone_number_field',
                   'password1',
-                  'password2'
+                  'password2',
+                  'phone_number_field',
                   )
 
     def save(self, commit=True):
