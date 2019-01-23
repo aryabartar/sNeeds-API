348b730e4610fa83c6cb8b991739e6a0ae110448
arya <a.khaligh@aut.ac.ir>
2019-01-23 19:14:17 +0330

    Modify search bar

--- search/views.py
+++ search/views.py
@@ -3,8 +3,14 @@ from website.models import Booklet
 
 
 # Create your views here.
-def search_booklet(request, search_text):
-    search_qs_result = None
-    search_qs_result = Booklet.objects.filter(title__contains=search_text)
+def search_booklet(request):
+    context = {}
+    if request.method == "GET":
+        search_text = request.GET['value']
+        search_qs_result = None
+        search_qs_result = Booklet.objects.filter(title__contains=search_text)
 
-    return render(request, "search/search.html")
+        context["booklets"] = search_qs_result
+        context["search_text"] = "نتایج جستجو برای : {}".format(search_text)
+
+    return render(request, "search/search.html", context=context)
