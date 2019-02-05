b45bc8e2faa49b8b461394d9e8037802ab901424
arya <a.khaligh@aut.ac.ir>
2019-02-05 21:37:09 +0330

    Made project ready for deployement

--- sneeds/settings/local.py
+++ sneeds/settings/local.py
@@ -148,3 +148,13 @@ HAYSTACK_CONNECTIONS = {
         'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
     },
 }
+
+CORS_REPLACE_HTTPS_REFERER = False
+HOST_SCHEME = "http://"
+SECURE_PROXY_SSL_HEADER = None
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SECURE_HSTS_SECONDS = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_FRAME_DENY = False
