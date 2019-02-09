9bef957f9905a0d90d26acf9666915e3bfaf79c2
arya <a.khaligh@aut.ac.ir>
2019-02-09 12:47:55 +0330

    Modify TopicSerializer

--- booklet/serializers.py
+++ booklet/serializers.py
@@ -24,32 +24,33 @@ class FieldSerializer(serializers.ModelSerializer):
 
 
 class TopicSerializerWhitNoBooklet(serializers.ModelSerializer):
-    field = serializers.SerializerMethodField()
     topic_url = serializers.SerializerMethodField()
-    field_url = serializers.SerializerMethodField()
-
-    def get_field_url(self, topic):
-        request = self.context.get('request')
-        field_url = topic.field.get_absolute_url()
-        return request.build_absolute_uri(field_url)
 
     def get_topic_url(self, topic):
         request = self.context.get('request')
         topic_url = topic.get_absolute_url()
         return request.build_absolute_uri(topic_url)
 
-    def get_field(self, topic):
-        return topic.field.title
-
     class Meta:
         model = BookletTopic
         fields = [
-            'title', 'topic_url', 'field', 'field_url', 'slug',
+            'title', 'topic_url', 'slug',
         ]
 
 
 class TopicSerializer(TopicSerializerWhitNoBooklet):
+    field = serializers.SerializerMethodField()
+    field_url = serializers.SerializerMethodField()
+
     topic_booklets = serializers.SerializerMethodField()
+
+    def get_field_url(self, topic):
+        request = self.context.get('request')
+        field_url = topic.field.get_absolute_url()
+        return request.build_absolute_uri(field_url)
+
+    def get_field(self, topic):
+        return topic.field.title
 
     def get_topic_booklets(self, topic):
         topic_booklets = topic.booklets.all()
