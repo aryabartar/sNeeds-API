ea4f5f27e50b6fa1fcab758fb5806fc4be563428
arya <a.khaligh@aut.ac.ir>
2019-02-10 19:34:28 +0330

    Modify cafe model

--- discounts/models.py
+++ discounts/models.py
@@ -12,15 +12,15 @@ User = get_user_model()
 
 
 class Cafe(models.Model):
-    name = models.CharField(max_length=128, null=True, blank=True)
-    information = models.TextField(null=True, blank=True)
-    address = models.CharField(max_length=256, blank=False, default="تهران")
-    phone_number = models.CharField(max_length=128, blank=True, null=True)
-    slug = models.SlugField(max_length=128, blank=False, unique=True, default="default")
+    name = models.CharField(max_length=128, null=False, blank=False)
+    information = models.TextField(null=False, blank=False)
+    address = models.CharField(max_length=256, blank=False, help_text="In a specific format.")
+    phone_number = models.CharField(max_length=128, blank=False, null=False)
+    slug = models.SlugField(max_length=128, blank=False, unique=True)
 
-    def get_absolute_url(self):
-        """Returns the url to access a detail record for this book."""
-        return reverse('discounts:cafe_page', args=[str(self.slug)])
+    # def get_absolute_url(self):
+    #     """Returns the url to access a detail record for this book."""
+    #     return reverse('discounts:cafe_page', args=[str(self.slug)])
 
     def __str__(self):
         return self.name
