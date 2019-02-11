aed3fb0e63dab38289d9d36e79964f80788d5ee1
arya <a.khaligh@aut.ac.ir>
2019-02-11 19:20:31 +0330

    Add jwt token

--- sneeds/settings/restconf/main.py
+++ sneeds/settings/restconf/main.py
@@ -1,6 +1,7 @@
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
