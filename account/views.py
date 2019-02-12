0b160e1d93388ef09ee380a5ec2a1dd2bdcc7a01
arya <a.khaligh@aut.ac.ir>
2019-02-12 22:20:13 +0330

    Update views

--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, get_user_model
+from django.db.models import Q
 
 from rest_framework import permissions
 from rest_framework.views import APIView
@@ -8,6 +9,8 @@ from rest_framework_jwt.settings import api_settings
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
+
+User = get_user_model()
 
 
 class AuthView(APIView):
@@ -21,7 +24,19 @@ class AuthView(APIView):
         username = data.get('username')
         password = data.get('password')
         user = authenticate(username=username, password=password)
-        payload = jwt_payload_handler(user)
-        token = jwt_encode_handler(payload)
-        response = jwt_response_payload_handler(token, user, request=request)
-        return Response(response)
+
+        qs = User.objects.filter(
+            Q(username__exact=username)|
+            Q(email__exact=username)
+        )
+        if len(qs) == 1:
+            user_obj = qs.first()
+            if user_obj.check_password(password):
+                user = user_obj
+
+                payload = jwt_payload_handler(user)
+                token = jwt_encode_handler(payload)
+                response = jwt_response_payload_handler(token, user, request=request)
+                return Response(response)
+
+        return Response({"detail": "Invalid credentails!"}, status=401)
--- scripts/test_api.py
+++ scripts/test_api.py
@@ -14,11 +14,12 @@ account_data = {
 
 headers = {
     "Content-Type": "application/json",
+    "Authorization": "JWT " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo5LCJ1c2VybmFtZSI6InRlc3RhcGkiLCJleHAiOjE1NDk5OTc1MTEsImVtYWlsIjoiIiwib3JpZ19pYXQiOjE1NDk5OTcyMTF9.WzzutV_0FzkM5r3GiYuRg1e991M_TqN8BxbGUOswJKs"
 }
 
 r = requests.post(AUTH_ENDPOINT, data=json.dumps(account_data), headers=headers)
 # token = r.json()#['token']
-print(r.text)
+print(r.json())
 # print(token)
 
 # headers = {
