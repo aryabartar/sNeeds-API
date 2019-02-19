b2608e4f114e830afe857b2d5d34673080d98190
arya <a.khaligh@aut.ac.ir>
2019-02-19 16:55:50 +0330

    Update booklet urls

--- scripts/test_api.py
+++ scripts/test_api.py
@@ -7,8 +7,8 @@ REFRESH_ENDPOINT = AUTH_ENDPOINT + "refresh/"
 ENDPOINT = "http://127.0.0.1:8000/cafe/user-discounts/"
 
 account_data = {
-    "username": "testapi15",
-    "email": "testapi15@gmail.com",
+    "username": "testapi1544",
+    "email": "testapi1544@gmail.com",
     "password": "Sneeds@203040",
     "password2": "Sneeds@203040",
 
