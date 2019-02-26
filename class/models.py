f59fc66309824e25d262c4aab4240dc6aae4608d
arya <a.khaligh@aut.ac.ir>
2019-02-26 17:43:50 +0330

    Add class model

--- class/models.py
+++ class/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+User = get_user_model()
+
+
+class PublicClass(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.IntegerField(help_text="120000 stands for 120 hezar tooman.")
+
+    def __str__(self):
+        return self.title
+
+
+class SoldPublicClass(models.Model):
+    public_class = models.ForeignKey(PublicClass, on_delete=models.SET_NULL, related_name="sold_classes")
+    user = get_user_model()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{user} | {public_class}".format(user=self.user, public_class=self.public_class)
