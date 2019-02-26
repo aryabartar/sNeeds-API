7abe6db7f3ea52466c8e0bcfdfe0245e9ab5a5cb
arya <a.khaligh@aut.ac.ir>
2019-02-26 22:27:47 +0330

    Update RequestPage view

--- payment/views.py
+++ payment/views.py
@@ -11,14 +11,14 @@ from classes.models import PublicClass
 
 MERCHANT = 'd40321dc-8bb0-11e7-b63c-005056a205be'
 client = Client('https://www.zarinpal.com/pg/services/WebGate/wsdl')
-amount = 1000  # Toman / Required
+# amount = 1000  # Toman / Required
 description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
 email = 'email@example.com'  # Optional
 mobile = '09011353909'  # Optional
 CallbackURL = 'http://127.0.0.1:8000/payment/verify/'  # Important: need to edit for realy server.
 
 
-def send_request(request):
+def send_request(request, amount):
     result = client.service.PaymentRequest(MERCHANT, amount, description, email, mobile, CallbackURL)
     if result.Status == 100:
         return redirect('https://www.zarinpal.com/pg/StartPay/' + str(result.Authority))
@@ -43,18 +43,27 @@ def verify(request):
 class RequestPage(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_public_class(self, public_class_slug):
+        public_class = PublicClass.objects.filter(slug__exact=public_class_slug)
+        if public_class.exists():
+            return public_class[0]
+        return None
+
     def get(self, request, *args, **kwargs):
         # return send_request(request)
         return Response({})
 
     def post(self, request, *args, **kwargs):
         data = request.data
-        if 'public_class' in data:
-            public_class = PublicClass.objects.filter(slug__iexact=data['public_class'])
-            print(data)
-            if public_class.exists():
-                public_class = public_class[0]
-                return Response({"title": public_class.title})
-            else:
-                return Response({"message": "Bag is empty!"})
-        return Response({})
+        if 'just_show' in data:
+            if 'public_class' in data:
+                public_class = self.get_public_class(data['public_class'])
+                if public_class is not None:
+                    if data['just_show'] == "true":
+                        return Response({"title": public_class.title, "price": public_class.price})
+                    else:
+                        return send_request(request, amount=public_class.price)
+                else:
+                    return Response({"message": "No public_class found!"})
+
+        return Response({"message": "Bad request format!"})
