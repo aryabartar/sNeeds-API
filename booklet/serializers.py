c434ae6eac941101055ec25ea7d8e468c7a0dad4
arya <a.khaligh@aut.ac.ir>
2019-02-27 14:26:49 +0330

    Aff topic And field to serializer

--- booklet/serializers.py
+++ booklet/serializers.py
@@ -18,12 +18,14 @@ class FieldSerializer(serializers.ModelSerializer):
 
 class TopicSerializer(serializers.ModelSerializer):
     topic_url = serializers.SerializerMethodField()
-    field = serializers.SerializerMethodField()
+    field_title = serializers.SerializerMethodField()
+    field_slug = serializers.SerializerMethodField()
 
-    def get_field(self, topic):
-        request = self.context.get('request')
-        field_url = topic.field.get_absolute_url()
-        return request.build_absolute_uri(field_url)
+    def get_field_title(self, topic):
+        return topic.field.title
+
+    def get_field_slug(self, topic):
+        return topic.field.slug
 
     def get_topic_url(self, topic):
         request = self.context.get('request')
@@ -39,6 +41,22 @@ class BookletSerializer(serializers.ModelSerializer):
     booklet_url = serializers.SerializerMethodField()
     tags = serializers.SerializerMethodField()
     topic = serializers.SerializerMethodField()
+    field_title = serializers.SerializerMethodField()
+    field_slug = serializers.SerializerMethodField()
+    topic_title = serializers.SerializerMethodField()
+    topic_slug = serializers.SerializerMethodField()
+
+    def get_field_title(self, booklet):
+        return booklet.topic.field.title
+
+    def get_field_slug(self, booklet):
+        return booklet.topic.field.slug
+
+    def get_topic_title(self, booklet):
+        return booklet.topic.title
+
+    def get_topic_slug(self, booklet):
+        return booklet.topic.slug
 
     def get_tags(self, booklet):
         tags = booklet.tags.all()
