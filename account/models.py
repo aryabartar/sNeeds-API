1aadcaea70b7fef9e831efac23b242b47dfb68da
arya <a.khaligh@aut.ac.ir>
2019-03-05 16:15:27 +0330

    Modify UserInformation

--- account/models.py
+++ account/models.py
@@ -7,7 +7,9 @@ User = get_user_model()
 
 class UserInformation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name="user_information")
-    phone = models.CharField(max_length=11)
+    phone = models.CharField(max_length=11, null=True, blank=True)
+    field = models.CharField(max_length=120 , null=True , blank=True)
+    university = models.CharField(max_length=120, null=True, blank=True)
 
     def __str__(self):
         return "{} information".format(self.user)
