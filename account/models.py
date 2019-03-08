fea7b7ccad4ad8998a53b8b536319264901415e0
arya <a.khaligh@aut.ac.ir>
2019-03-08 21:30:53 +0330

    Add Order model

--- account/models.py
+++ account/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 User = get_user_model()
 
--- order/models.py
+++ order/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from cart.models import Cart
+
+User = get_user_model()
+
+ORDER_STATUS_CHOICES = (
+    ("created", "Created"),
+    ("paid", "Paid"),
+    ("refunded", "Refunded"),
+)
+
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=120, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    cart = models.ForeignKey(Cart, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=128, default="created")
+    total = models.DecimalField(default=0, max_digits=20, decimal_places=0)
+
+    def __str__(self):
+        return self.order_id
