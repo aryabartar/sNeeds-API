e2a2a097f31f787819905e9b8685767c1dc53b1d
arya <a.khaligh@aut.ac.ir>
2019-03-08 22:14:11 +0330

    Update Order model

--- order/models.py
+++ order/models.py
@@ -14,9 +14,9 @@ ORDER_STATUS_CHOICES = (
 
 class Order(models.Model):
     order_id = models.CharField(max_length=120, blank=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
-    cart = models.ForeignKey(Cart, on_delete=models.SET_NULL)
-    status = models.CharField(max_length=128, default="created")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    cart = models.ForeignKey(Cart, on_delete=models.SET_NULL, null=True)
+    status = models.CharField(max_length=128, default="created", choices=ORDER_STATUS_CHOICES)
     total = models.DecimalField(default=0, max_digits=20, decimal_places=0)
 
     def __str__(self):
--- sneeds/base.py
+++ sneeds/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'account',
     'cart',
     'classes',
+    'order',
 
 ]
 
