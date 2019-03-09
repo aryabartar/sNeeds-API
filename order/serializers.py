cbdca8b5274eef08cc89eb75c3fe48f2772a3a79
arya <a.khaligh@aut.ac.ir>
2019-03-09 09:34:07 +0330

    Update views

---
+++ order/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"
