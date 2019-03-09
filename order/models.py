0839a2e746b80c4f7eb053ef77e49b6d636721fd
arya <a.khaligh@aut.ac.ir>
2019-03-09 09:27:54 +0330

    Add post_save_cart_total

--- order/models.py
+++ order/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 
 from sneeds.utils import unique_order_id_generator
 from cart.models import Cart
@@ -21,6 +21,12 @@ class Order(models.Model):
     status = models.CharField(max_length=128, default="created", choices=ORDER_STATUS_CHOICES)
     total = models.DecimalField(default=0, max_digits=20, decimal_places=0)
 
+    def update_total(self):
+        cart_total = self.cart.total
+        self.total = cart_total
+        self.save()
+        return self.total
+
     def __str__(self):
         return self.order_id
 
@@ -30,4 +36,22 @@ def pre_save_create_order_id(sender, instance, *args, **kwargs):
         instance.order_id = unique_order_id_generator(instance)
 
 
+def post_save_cart_total(sender, instance, created, *args, **kwargs):
+    if not created:
+        cart_obj = instance
+        cart_id = cart_obj.id
+        qs = Order.objects.filter(cart_id=cart_id)
+        if qs.exists():
+            if qs.count() == 1:
+                order_obj = qs.first()
+                order_obj.update_total()
+
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+    if created:
+        instance.update_total()
+
+
 pre_save.connect(pre_save_create_order_id, sender=Order)
+post_save.connect(post_save_cart_total, sender=Cart)
+post_save.connect(post_save_order, Order)
--- sneeds/utils.py
+++ sneeds/utils.py
@@ -2,7 +2,7 @@ import random
 import string
 
 
-def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+def random_string_generator(size=6, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
