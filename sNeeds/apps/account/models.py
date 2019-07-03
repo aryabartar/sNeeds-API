346f8a885af7d55802ea314c8c01b2fbf7d25bda
Arya <bartararya@gmail.com>
2019-07-03 19:04:48 +0430

    Add __str__ to models

--- sNeeds/apps/account/models.py
+++ sNeeds/apps/account/models.py
@@ -5,16 +5,25 @@ from django.contrib.auth.models import User
 class Country (models.Model):
     name = models.CharField(max_length=256, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class University (models.Model):
     name = models.CharField(max_length=256, unique=True)
     country = models.CharField(max_length=256)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class FieldOfStudy (models.Model):
     name = models.CharField(max_length=256, unique=True)
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class ConsultantProfile (models.Model):
@@ -23,3 +32,6 @@ class ConsultantProfile (models.Model):
     university = models.ManyToManyField(University)
     field_of_study = models.ManyToManyField(FieldOfStudy)
     country = models.ManyToManyField(Country)
+
+    def __str__(self):
+        return self.consultant.__str__()
