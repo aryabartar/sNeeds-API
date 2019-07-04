5b9ddc9b13b7767d49806b291d4099202206c004
Arya <bartararya@gmail.com>
2019-07-04 20:04:52 +0430

    Add serializers

--- sNeeds/apps/account/models.py
+++ sNeeds/apps/account/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Country (models.Model):
+class Country(models.Model):
     name = models.CharField(max_length=256, unique=True)
 
     def __str__(self):
         return self.name
 
 
-class University (models.Model):
+class University(models.Model):
     name = models.CharField(max_length=256, unique=True)
     country = models.CharField(max_length=256)
     description = models.TextField(blank=True, null=True)
@@ -18,20 +18,24 @@ class University (models.Model):
         return self.name
 
 
-class FieldOfStudy (models.Model):
+class FieldOfStudy(models.Model):
     name = models.CharField(max_length=256, unique=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
+        self.name = self.name
         return self.name
 
 
-class ConsultantProfile (models.Model):
+class ConsultantProfile(models.Model):
     consultant = models.OneToOneField(
-        User, on_delete=models.SET_NULL,  null=True)
+        User, on_delete=models.SET_NULL, null=True)
     university = models.ManyToManyField(University)
     field_of_study = models.ManyToManyField(FieldOfStudy)
     country = models.ManyToManyField(Country)
+    slug = models.SlugField(help_text="lowercase pls")
+    profile_picture = models.ImageField(upload_to="consultant_profile_photo")
+    aparat_link = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.consultant.__str__()
