180057b156dc6eba4559fa04ba40e073016143fa
Arya <bartararya@gmail.com>
2019-07-06 08:19:36 +0430

    Add TimeSlotSale model

--- sNeeds/apps/store/models.py
+++ sNeeds/apps/store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from sNeeds.apps.account.models import ConsultantProfile
+
 
 # Create your models here.
+class TimeSlotSale(models.Model):
+    consultant = models.ForeignKey(ConsultantProfile, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    price = models.IntegerField()
