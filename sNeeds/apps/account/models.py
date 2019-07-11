aa76a9ccb0c38a8a6eff15c1bb4b26b25507534c
Arya <bartararya@gmail.com>
2019-07-11 22:03:12 +0430

    Add auth app

--- sNeeds/apps/account/models.py
+++ sNeeds/apps/account/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 
+from django.contrib.auth.models import User
+
 class Country(models.Model):
     name = models.CharField(max_length=256, unique=True)
     slug = models.SlugField(help_text="Lowercase pls")
