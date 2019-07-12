3caabc93e68c04206225da6e996124a4b5e01b5f
Arya <bartararya@gmail.com>
2019-07-12 09:56:54 +0430

    Changed JWT payload handler.

--- sNeeds/settings/JWTAuthConfig.py
+++ sNeeds/settings/JWTAuthConfig.py
@@ -16,7 +16,7 @@ JWT_AUTH = {
     'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
 
     'JWT_RESPONSE_PAYLOAD_HANDLER':
-    'rest_framework_jwt.utils.jwt_response_payload_handler',
+    'sNeeds.apps.customAuth.utils.jwt_response_payload_handler',
 
     'JWT_SECRET_KEY': SECRET_KEY,
     'JWT_GET_USER_SECRET_KEY': None,
