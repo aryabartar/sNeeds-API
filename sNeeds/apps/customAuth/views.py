904cff29282f528bb9a3e1aa98a6f8dc1f1290d2
Arya <bartararya@gmail.com>
2019-07-12 13:02:27 +0430

    Remved JWT verify

--- sNeeds/apps/customAuth/views.py
+++ sNeeds/apps/customAuth/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 
 from rest_framework_jwt import utils as jwt_utils
 
-
+from .utils import jwt_response_payload_handler
 
 class AuthView(APIView):
     '''
@@ -34,10 +34,8 @@ class AuthView(APIView):
         if user:
             payload = jwt_utils.jwt_payload_handler(user)
             token = jwt_utils.jwt_encode_handler(payload)
-            response = jwt_utils.jwt_response_payload_handler(token, user=user, request=request)
+            response = jwt_response_payload_handler(token, user=user, request=request)
 
-            expires = {'expires': timezone.now() + settings.JWT_AUTH['JWT_REFRESH_EXPIRATION_DELTA'] - datetime.timedelta(seconds=200)}
-            response.update(expires)
             return Response(response, status=200)
 
         else:
