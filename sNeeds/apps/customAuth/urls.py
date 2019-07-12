3b9154a085f53f02ad6c9ec214c6d50e260cd54c
Arya <bartararya@gmail.com>
2019-07-12 12:19:55 +0430

    Add custom token view

--- sNeeds/apps/customAuth/urls.py
+++ sNeeds/apps/customAuth/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+
+from .views import AuthView
+
 
 app_name = "auth"
 
 urlpatterns = [
-    path('jwt/token/', obtain_jwt_token),
+    path('jwt/token/', AuthView.as_view()),
     path('jwt/token/refresh/', refresh_jwt_token),
 ]
--- sNeeds/apps/customAuth/views.py
+++ sNeeds/apps/customAuth/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from rest_framework_jwt import utils as jwt_utils
+
+from .utils import jwt_response_payload_handler
+
+
+class AuthView(APIView):
+    "hh"
+    permission_classes = []
+
+    def post(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated:
+            return Response({'detail': 'You are already authenticated'}, status=400)
+
+        data = request.data
+        email = data.get('email')
+        password = data.get('password')
+        user = authenticate(email=email, password=password)
+
+        payload = jwt_utils.jwt_payload_handler(user)
+        token = jwt_utils.jwt_encode_handler(payload)
+        response = jwt_response_payload_handler(token, user=user, request=request)
+        return Response(response, status=200)
