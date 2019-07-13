9bc0bdd21c9e469e4aafc1ce10ab64e0242f2f08
Arya <bartararya@gmail.com>
2019-07-13 12:33:20 +0430

    Update ALLOWED HOSTS

--- sNeeds/settings/development.py
+++ sNeeds/settings/development.py
@@ -25,7 +25,7 @@ from .keys import SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['193.176.241.131' , '127.0.0.1']
 
 # Application definition
 
