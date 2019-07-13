8010ef44cb5c5787d6d6b4c7d61fd4db295abfe9
Arya <bartararya@gmail.com>
2019-07-13 22:18:03 +0430

    Change TimeSlotSailFilter filter

--- sNeeds/apps/store/filtersets.py
+++ sNeeds/apps/store/filtersets.py
@@ -1,13 +1,12 @@
 from django_filters import rest_framework as filters
 
-from sNeeds.apps.account.models import ConsultantProfile
 from . import models
 
 
 class TimeSlotSailFilter(filters.FilterSet):
-    consultant = filters.ModelMultipleChoiceFilter(field_name='consultant', to_field_name='slug',
-                                                          queryset=ConsultantProfile.objects.all(), label="slug")
+    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
 
     class Meta:
         model = models.TimeSlotSale
-        fields = ['consultant', 'start_time', 'end_time', 'price']
+        fields = ['consultant', 'start_time', 'end_time', 'min_price', 'max_price', 'price']
