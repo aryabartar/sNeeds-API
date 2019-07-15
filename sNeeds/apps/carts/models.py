ac81eb5a729ebbbb3c685309489adaca4db892a7
Arya <bartararya@gmail.com>
2019-07-15 10:32:27 +0430

    Add Cart model.

--- sNeeds/apps/carts/models.py
+++ sNeeds/apps/carts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from sNeeds.apps.store.models import TimeSlotSale
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_slots = models.ManyToManyField(TimeSlotSale)
+    total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
