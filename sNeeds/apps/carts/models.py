7df677b3d34a5fa133d748f66429cbef6f8d9284
Arya <bartararya@gmail.com>
2019-07-16 21:22:14 +0430

    Add pre_save_cart_receiver

--- sNeeds/apps/carts/models.py
+++ sNeeds/apps/carts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import pre_save, post_save, m2m_changed
 
 from sNeeds.apps.store.models import TimeSlotSale
 
@@ -8,10 +9,23 @@ User = get_user_model()
 
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
-    time_slot_sales = models.ManyToManyField(TimeSlotSale)
+    time_slot_sales = models.ManyToManyField(TimeSlotSale, blank=True)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "User {} cart".format(self.user)
+
+
+def pre_save_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        time_slot_sales = instance.time_slot_sales.all()
+        total = 0
+        for t in time_slot_sales:
+            total = t.price
+        instance.total = total
+        instance.save()
+
+
+m2m_changed.connect(pre_save_cart_receiver, sender=Cart.time_slot_sales.through)
