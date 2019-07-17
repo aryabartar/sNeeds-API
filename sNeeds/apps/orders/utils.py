e6f2c05a278cdf614b0c3cb4a542e6f3d04d209a
Arya <bartararya@gmail.com>
2019-07-17 20:20:14 +0430

    Add utils

---
+++ sNeeds/apps/orders/utils.py
@@ -0,0 +1,21 @@
+import datetime
+import os
+import random
+import string
+
+
+def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def unique_order_id_generator(instance):
+    """
+    This is for a Django project with an order_id field
+    """
+    order_new_id = random_string_generator()
+
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(order_id=order_new_id).exists()
+    if qs_exists:
+        return unique_order_id_generator(instance)
+    return order_new_id
