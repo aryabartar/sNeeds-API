62f460dd008ca8d25e539988e0767a468544bc42
Arya <bartararya@gmail.com>
2019-07-17 20:22:47 +0430

    Add pre_save_create_order_id

--- sNeeds/apps/orders/models.py
+++ sNeeds/apps/orders/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save
 
+from .utils import unique_order_id_generator
 from sNeeds.apps.carts.models import Cart
 
 ORDER_STATUS_CHOICES = (
@@ -10,7 +12,15 @@ ORDER_STATUS_CHOICES = (
 
 
 class Order(models.Model):
-    order_id = models.CharField(max_length=12)
+    order_id = models.CharField(max_length=12, blank=True)
     cart = models.ForeignKey(Cart, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=256, default='created', choices=ORDER_STATUS_CHOICES)
     total = models.DecimalField(max_digits=100, decimal_places=2)
+
+
+def pre_save_create_order_id(sender, instance, *args, **kwargs):
+    if not instance.order_id:
+        instance.order_id = unique_order_id_generator(instance)
+
+
+pre_save.connect(pre_save_create_order_id, sender=Order)
