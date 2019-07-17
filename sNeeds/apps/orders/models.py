d78bde6b6e1eb85213aaee18e3b0329fca48006c
Arya <bartararya@gmail.com>
2019-07-17 20:41:35 +0430

    Add post_save_cart_total

--- sNeeds/apps/orders/models.py
+++ sNeeds/apps/orders/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 
 from .utils import unique_order_id_generator
 from sNeeds.apps.carts.models import Cart
@@ -15,7 +15,16 @@ class Order(models.Model):
     order_id = models.CharField(max_length=12, blank=True, help_text="Leave this field blank.")
     cart = models.ForeignKey(Cart, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=256, default='created', choices=ORDER_STATUS_CHOICES)
-    total = models.DecimalField(max_digits=100, decimal_places=2)
+    total = models.DecimalField(max_digits=100, decimal_places=2, null=True, default=0, blank=True)
+
+    def __str__(self):
+        return str(self.order_id)
+
+    def update_total(self):
+        self.total = self.cart.total
+        print("HERE" , self.cart.total)
+        self.save()
+        return self.total
 
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
@@ -23,4 +32,20 @@ def pre_save_create_order_id(sender, instance, *args, **kwargs):
         instance.order_id = unique_order_id_generator(instance)
 
 
+def post_save_cart_total(sender, instance, created, *args, **kwargs):
+    if not created:
+        cart_obj = instance
+        qs = Order.objects.filter(cart=cart_obj)
+        if qs.count() == 1:
+            order_obj = qs.first()
+            order_obj.update_total()
+
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+    if created:
+        instance.update_total()
+
+
 pre_save.connect(pre_save_create_order_id, sender=Order)
+post_save.connect(post_save_order, sender=Order)
+post_save.connect(post_save_cart_total, sender=Cart)
