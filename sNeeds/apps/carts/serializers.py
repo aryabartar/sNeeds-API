9a05878326ac1858cfffbb9e952abeff326c1017
Arya <bartararya@gmail.com>
2019-07-18 18:42:57 +0430

    Remove TODO'

--- sNeeds/apps/carts/serializers.py
+++ sNeeds/apps/carts/serializers.py
@@ -3,14 +3,7 @@ from rest_framework import serializers
 from .models import Cart
 
 
-# TODO : Add time_slot_sales validator for invalid data.
 class CartSerializer(serializers.ModelSerializer):
-    time_slot_sales = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        lookup_field='id',
-        view_name='store:time-slot-sale-detail'
-    )
 
     class Meta:
         model = Cart
@@ -20,7 +13,6 @@ class CartSerializer(serializers.ModelSerializer):
             'user': {'read_only': True},
             'total': {'read_only': True},
         }
-
 
     def validate(self, data):
         request = self.context.get("request", None)
@@ -35,7 +27,6 @@ class CartSerializer(serializers.ModelSerializer):
         request = self.context.get('request', None)
         if request and hasattr(request, "user"):
             user = request.user
-
-        time_slot_sales = request.data.get('time_slot_sales', [])
+        time_slot_sales = validated_data.get('time_slot_sales', [])
         cart_obj = Cart.objects.get_new_and_deactive_others(user, time_slot_sales=time_slot_sales)
         return cart_obj
