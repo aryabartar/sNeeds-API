740f4582f8ca5792eb7662228f781146ace18156
Arya <bartararya@gmail.com>
2019-07-19 10:11:10 +0430

    Add payments view

--- sNeeds/apps/payments/views.py
+++ sNeeds/apps/payments/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from zeep import Client
+
+MERCHANT = 'd40321dc-8bb0-11e7-b63c-005056a205be'
+client = Client('https://www.zarinpal.com/pg/services/WebGate/wsdl')
+amount = 1000  # Toman / Required
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+email = 'email@example.com'  # Optional
+mobile = '09123456789'  # Optional
+CallbackURL = 'http://localhost:8000/verify/'  # Important: need to edit for realy server.
+
+
+def send_request(request):
+    result = client.service.PaymentRequest(MERCHANT, amount, description, email, mobile, CallbackURL)
+    if result.Status == 100:
+        return redirect('https://www.zarinpal.com/pg/StartPay/' + str(result.Authority))
+    else:
+        return HttpResponse('Error code: ' + str(result.Status))
+
+
+def verify(request):
+    if request.GET.get('Status') == 'OK':
+        result = client.service.PaymentVerification(MERCHANT, request.GET['Authority'], amount)
+        if result.Status == 100:
+            return HttpResponse('Transaction success.\nRefID: ' + str(result.RefID))
+        elif result.Status == 101:
+            return HttpResponse('Transaction submitted : ' + str(result.Status))
+        else:
+            return HttpResponse('Transaction failed.\nStatus: ' + str(result.Status))
+    else:
+        return HttpResponse('Transaction failed or canceled by user')
