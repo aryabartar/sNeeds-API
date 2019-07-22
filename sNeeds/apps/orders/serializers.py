7e872d1dd13f630ea3621be6e31f2395946a9e8c
Arya <bartararya@gmail.com>
2019-07-22 21:28:08 +0430

    Update OrderSerializer'

--- sNeeds/apps/orders/serializers.py
+++ sNeeds/apps/orders/serializers.py
@@ -4,20 +4,19 @@ from rest_framework.exceptions import ValidationError
 from .models import Order
 
 from sNeeds.apps.carts.models import Cart
+from sNeeds.apps.carts.serializers import CartSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="order:order-detail", lookup_field='id', read_only=True)
-    cart_url = serializers.HyperlinkedIdentityField(view_name="cart:cart-detail", lookup_field='id', source='cart',
-                                                    read_only=True)
+    cart = CartSerializer(read_only=True)
 
     class Meta:
         model = Order
-        fields = ['id', 'url', 'order_id', 'cart', 'cart_url', 'status', 'total', ]
+        fields = ['id', 'url', 'order_id', 'cart', 'status', 'total', ]
         extra_kwargs = {
             'id': {'read_only': True},
             'order_id': {'read_only': True},
-            'cart': {'read_only': True},
             'status': {'read_only': True},
             'total': {'read_only': True},
         }
--- sNeeds/apps/orders/services.py
+++
@@ -1,6 +0,0 @@
-from sNeeds.apps.carts.models import Cart
-from sNeeds.apps.orders.models import Order, SoldCart
-
-
-def accept_order_pay(order):
-   cart = order.cart
