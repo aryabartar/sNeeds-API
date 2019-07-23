235689889dc3af2a2b5f71353cdb96d9a9a9de90
Arya <bartararya@gmail.com>
2019-07-23 16:46:40 +0430

    Add Comment model

--- sNeeds/apps/comments/models.py
+++ sNeeds/apps/comments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from sNeeds.apps.account.models import ConsultantProfile
+
+User = get_user_model()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    consultant = models.ForeignKey(ConsultantProfile, on_delete=models.CASCADE)
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "User : {} | Consultant : {}".format(str(self.user), str(self.consultant))
