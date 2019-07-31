5ff803b69213529996ae63be919d2e02a35df78c
Arya <bartararya@gmail.com>
2019-07-31 12:45:17 +0430

    Add comment number

--- sNeeds/apps/account/serializers.py
+++ sNeeds/apps/account/serializers.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from sNeeds.apps.comments.models import SoldTimeSlotRate
+from sNeeds.apps.comments.models import Comment
 
 from . import models
 
@@ -44,6 +45,7 @@ class ConsultantProfileSerializer(serializers.ModelSerializer):
     first_name = serializers.SerializerMethodField(read_only=True)
     last_name = serializers.SerializerMethodField(read_only=True)
     rate = serializers.SerializerMethodField(read_only=True)
+    comment_number = serializers.SerializerMethodField(read_only=True)
 
     universities = UniversitySerializer(many=True, read_only=True)
     field_of_studies = FieldOfStudySerializer(many=True, read_only=True)
@@ -54,7 +56,7 @@ class ConsultantProfileSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'url', 'bio', 'profile_picture', 'first_name', 'last_name',
             'universities', 'field_of_studies', 'countries', 'slug', 'aparat_link',
-            'resume', 'rate', 'active')
+            'resume', 'rate', 'comment_number', 'active')
 
     def get_rate(self, obj):
         qs = SoldTimeSlotRate.objects.filter(sold_time_slot__consultant=obj)
@@ -75,3 +77,6 @@ class ConsultantProfileSerializer(serializers.ModelSerializer):
     def get_last_name(self, obj):
         return obj.user.last_name
 
+    def get_comment_number(self, obj):
+        qs = Comment.objects.filter(consultant=obj)
+        return qs.count()
