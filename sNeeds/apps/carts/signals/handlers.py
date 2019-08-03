eeea3cab0323b959e05f8c9935ca0c23aaeee2fc
Arya <bartararya@gmail.com>
2019-08-03 18:27:00 +0430

    Update post_save_time_slot_receiver

--- sNeeds/apps/carts/signals/handlers.py
+++ sNeeds/apps/carts/signals/handlers.py
@@ -15,7 +15,7 @@ def update_price(instance):
 
 def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
     if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
-        instance = update_price(instance)
+        update_price(instance)
         instance.save()
 
 
--- sNeeds/apps/store/signals/handlers.py
+++ sNeeds/apps/store/signals/handlers.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, pre_delete, m2m_changed
+from django.db.models.signals import post_save, pre_delete, pre_save, m2m_changed
 
 from sNeeds.apps.carts.models import Cart
 from sNeeds.apps.store.models import TimeSlotSale, SoldTimeSlotSale
@@ -13,7 +13,7 @@ def pre_delete_time_slot_sale_receiver(sender, instance, *args, **kwargs):
     Cart.objects.filter(time_slot_sales=instance).remove_time_slot_sale(instance)
 
 
-def post_save_time_sold_sold_receiver(sender, instance, created, *args, **kwargs):
+def post_save_time_slot_sold_receiver(sender, instance, created, *args, **kwargs):
     if created:
         notify_sold_time_slot(
             instance.consultant.user.email,
@@ -22,5 +22,13 @@ def post_save_time_sold_sold_receiver(sender, instance, created, *args, **kwargs
         )
 
 
+def post_save_time_slot_receiver(sender, instance, *args, **kwargs):
+    cart_qs = Cart.objects.filter(time_slot_sales=instance)
+    for obj in cart_qs:
+        obj.save()
+
+
 pre_delete.connect(pre_delete_time_slot_sale_receiver, sender=TimeSlotSale)
-post_save.connect(post_save_time_sold_sold_receiver, sender=SoldTimeSlotSale)
+
+post_save.connect(post_save_time_slot_receiver, sender=TimeSlotSale)
+post_save.connect(post_save_time_slot_sold_receiver, sender=SoldTimeSlotSale)
