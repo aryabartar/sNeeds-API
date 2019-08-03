6640ce175c188691dddae9e69276af637aca0bf1
Arya <bartararya@gmail.com>
2019-08-03 17:30:47 +0430

    Add post_save_time_sold_sold_receiver

--- sNeeds/apps/store/signals/handlers.py
+++ sNeeds/apps/store/signals/handlers.py
@@ -1,7 +1,7 @@
-from django.db.models.signals import pre_save, pre_delete, m2m_changed
+from django.db.models.signals import post_save, pre_delete, m2m_changed
 
 from sNeeds.apps.carts.models import Cart
-from sNeeds.apps.store.models import TimeSlotSale
+from sNeeds.apps.store.models import TimeSlotSale, SoldTimeSlotSale
 
 
 def pre_delete_time_slot_sale_receiver(sender, instance, *args, **kwargs):
@@ -12,4 +12,10 @@ def pre_delete_time_slot_sale_receiver(sender, instance, *args, **kwargs):
     Cart.objects.filter(time_slot_sales=instance).remove_time_slot_sale(instance)
 
 
+def post_save_time_sold_sold_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        print("heh")
+
+
 pre_delete.connect(pre_delete_time_slot_sale_receiver, sender=TimeSlotSale)
+post_save.connect(post_save_time_sold_sold_receiver, sender=SoldTimeSlotSale)
