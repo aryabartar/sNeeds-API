7c62c66021569dedf02a6a6deeb83999ae720b5f
eng.mrgh <mohammadrezaghofrany@gmail.com>
2019-08-20 16:18:19 +0430

    Now the in the admin page title and text doesn't shortened.

--- sNeeds/apps/ticket/admin.py
+++ sNeeds/apps/ticket/admin.py
@@ -2,35 +2,11 @@ from django.contrib import admin
 from .models import TicketMessage, Ticket
 
 
-def text_shorter(obj):
-    text_splited = obj.text.split(' ')
-    text = ' '.join(text_splited[:5])
-    text += "..."
-    return text
-text_shorter.short_description = 'text'
-
-
-def title_shorter_for_ticket_message(obj):
-    title_splited = obj.ticket.title.split(' ')
-    title = ' '.join(title_splited[:5])
-    title += "..."
-    return title
-title_shorter_for_ticket_message.short_description = 'title'
-
-
 @admin.register(TicketMessage)
 class ConsultantDiscountAdmin(admin.ModelAdmin):
-    list_display = ('id', text_shorter, title_shorter_for_ticket_message, 'created')\
-
-
-def title_shorter_for_ticket(obj):
-    title_splited = obj.title.split(' ')
-    title = ' '.join(title_splited[:5])
-    title += "..."
-    return title
-title_shorter_for_ticket.short_description = 'title'
+    list_display = ('id', 'text', 'title', 'created')
 
 
 @admin.register(Ticket)
 class ConsultantDiscountAdmin(admin.ModelAdmin):
-    list_display = ('id', title_shorter_for_ticket, 'user', 'consultant', 'created')
+    list_display = ('id', 'title', 'user', 'consultant', 'created')
