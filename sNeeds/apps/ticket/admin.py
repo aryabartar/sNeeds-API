e0b42e5d8f85b6ecbf2e5857bf462651324e1279
eng.mrgh <mohammadrezaghofrany@gmail.com>
2019-08-20 16:24:35 +0430

    Now the in the admin page title and text doesn't shortened. And I have tested and it is working.

--- sNeeds/apps/ticket/admin.py
+++ sNeeds/apps/ticket/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import TicketMessage, Ticket
 
 
+def get_title(obj):
+    return obj.ticket.title
+get_title.short_description = 'title'
+
+
 @admin.register(TicketMessage)
 class ConsultantDiscountAdmin(admin.ModelAdmin):
-    list_display = ('id', 'text', 'title', 'created')
+    list_display = ('id', 'text', get_title, 'created')
 
 
 @admin.register(Ticket)
