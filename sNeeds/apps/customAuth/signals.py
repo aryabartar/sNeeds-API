d8cd8987f287ae7c927b0840c60ce4a78e37621c
Arya <bartararya@gmail.com>
2019-08-20 11:05:17 +0430

    Add reset password to celery.

--- sNeeds/apps/customAuth/signals.py
+++ sNeeds/apps/customAuth/signals.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 from django_rest_passwordreset.signals import reset_password_token_created
 
-from sNeeds.utils import sendemail
+from .tasks import send_reset_password_email
 
 
 @receiver(reset_password_token_created)
@@ -21,7 +21,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     }
     reset_link = "http://193.176.241.131:8080/account/password-reset/?token={}".format(context['token'])
 
-    sendemail.reset_password(
+    send_reset_password_email.delay(
         context['email'],
         context['current_user'].first_name,
         reset_link,
---
+++ sNeeds/apps/customAuth/tasks.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+from celery import task, shared_task
+from celery.utils.log import get_task_logger
+
+from django.utils import timezone
+
+from sNeeds.utils import sendemail
+
+
+@task()
+def send_reset_password_email(email, first_name, reset_link):
+    sendemail.reset_password(
+        email,
+        first_name,
+        reset_link
+    )
+
