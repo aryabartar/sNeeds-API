5fe996ecef77f0eaa7cef23e1cea6f3e21f43712
Arya <bartararya@gmail.com>
2019-09-09 18:38:40 +0430

    Fix admin exists bug

--- code/web_entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "dfslkjeofiljrioejfior"
-python manage.py makemigrations
-python manage.py migrate
-exec "$@"
--- docker/python/web_entrypoint.sh
+++ docker/python/web_entrypoint.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
-echo "dfslkjeofiljrioejfior"
-python manage.py makemigrations
+# For running migrations
+echo " > Run migrations"
 python manage.py migrate
+
+# For creating admin
+echo " > Create admin"
+
+MAIL="a@g.com"
+PASS="111111"
+script="
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+
+email = '$MAIL';
+password = '$PASS';
+
+if User.objects.filter(email=email).count()==0:
+    User.objects.create_superuser(email, password);
+    User.objects.create_superuser(email, password);
+    print('Superuser created.');
+else:
+    print('Superuser creation skipped, user exists.');
+"
+printf "$script" | python manage.py shell
+
 exec "$@"
