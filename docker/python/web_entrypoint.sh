7cbba12500783d0fb61c3fc3cb74dab86122e739
Arya <bartararya@gmail.com>
2019-09-17 11:32:53 +0430

    Changed env vars

--- docker/python/web_entrypoint.sh
+++ docker/python/web_entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 # For running migrations
-echo " > Run migrations"
+echo " > Run migrations (bash)"
 python manage.py migrate
 
 
-
 # For creating admin
-echo " > Create admin"
+echo " > Create admin (bash)"
 
-MAIL="a@g.com"
-PASS="111111"
+MAIL=$SUPERUSER_USERNAME
+PASS=$SUPERUSER_PASSWORD
 script="
 from django.contrib.auth import get_user_model;
 User = get_user_model();
