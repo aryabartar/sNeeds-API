e8b98e6fc9bdf0b5da4f1936249c28d80a1380c3
Arya <bartararya@gmail.com>
2019-09-17 14:50:03 +0430

    Fix redis bug

--- code/sNeeds/settings/celery/celery_config.py
+++ code/sNeeds/settings/celery/celery_config.py
@@ -1,7 +1,7 @@
 from celery.schedules import crontab
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
