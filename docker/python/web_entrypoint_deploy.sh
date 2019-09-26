ebaea95d2d4a42c5c013d8d8c3645e340dfb5bd6
Arya <bartararya@gmail.com>
2019-09-26 19:17:07 +0330

    Remove create superuser script

--- docker/python/web_entrypoint_deploy.sh
+++ docker/python/web_entrypoint_deploy.sh
@@ -4,25 +4,4 @@ echo " > Run migrations (bash)"
 python manage.py migrate
 
 
-# For creating admin
-echo " > Create admin (bash)"
-
-MAIL=$SUPERUSER_USERNAME
-PASS=$SUPERUSER_PASSWORD
-script="
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-
-email = '$MAIL';
-password = '$PASS';
-
-if User.objects.filter(email=email).count()==0:
-    User.objects.create_superuser(email, password);
-    print('Superuser created.');
-else:
-    print('Superuser creation skipped, user exists.');
-"
-printf "$script" | python manage.py shell
-
-
 exec "$@"
