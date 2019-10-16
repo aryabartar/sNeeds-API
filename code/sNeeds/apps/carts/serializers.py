2e3d5855deeae5fba16e851746ff2ad1a6f93071
Mohammadreza Ghofrani <mohammadrezaghofrany@gmail.com>
2019-10-16 20:06:54 +0330

    Strong condition on selecting multiple time slots resolved

--- code/sNeeds/apps/carts/serializers.py
+++ code/sNeeds/apps/carts/serializers.py
@@ -16,7 +16,7 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = ['id', 'url', 'user', 'time_slot_sales', 'time_slot_sales_detail',
-                  'subtotal', 'count', 'total', ]
+                  'subtotal', 'total', ]
         extra_kwargs = {
             'id': {'read_only': True},
             'user': {'read_only': True},
@@ -48,8 +48,8 @@ class CartSerializer(serializers.ModelSerializer):
     def validate_time_slot_sales(self, list_of_sessions):
         sold_slots = SoldTimeSlotSale.objects.filter(sold_to=self.context.get('request').user).filter(used=False)
         for i in range(len(list_of_sessions)):
-            if (sold_slots.filter(start_time__lte=list_of_sessions[i].start_time).filter(end_time__gte=list_of_sessions[i].start_time)
-            or sold_slots.filter(start_time__lte=list_of_sessions[i].end_time).filter(end_time__gte=list_of_sessions[i].end_time)):
+            if (sold_slots.filter(start_time__lt=list_of_sessions[i].start_time).filter(end_time__gt=list_of_sessions[i].start_time)
+            or sold_slots.filter(start_time__lt=list_of_sessions[i].end_time).filter(end_time__gt=list_of_sessions[i].end_time)):
                 raise ValidationError(
                     {"detail": _("Time Conflict between %(selected_time_slot)d and %(sold_time_slot)d which is a bought session" % {"selected_time_slot": list_of_sessions[i].id, "sold_time_slot": sold_slots.first().id}),
                      "selected_time_slot": list_of_sessions[i].id,
@@ -58,8 +58,8 @@ class CartSerializer(serializers.ModelSerializer):
                 )
 
             for j in range(i+1, len(list_of_sessions)):
-                if list_of_sessions[j].start_time <= list_of_sessions[i].start_time <= list_of_sessions[j].end_time \
-                  or list_of_sessions[j].start_time <= list_of_sessions[i].end_time <= list_of_sessions[j].end_time:
+                if list_of_sessions[j].start_time < list_of_sessions[i].start_time < list_of_sessions[j].end_time \
+                  or list_of_sessions[j].start_time < list_of_sessions[i].end_time < list_of_sessions[j].end_time:
                     raise ValidationError(
                         {
                             "detail": _("Time Conflict between %(selected_time_slot_1)d and %(selected_time_slot_2)d" % {"selected_time_slot_1": list_of_sessions[i].id, "selected_time_slot_2": list_of_sessions[j].id}),
