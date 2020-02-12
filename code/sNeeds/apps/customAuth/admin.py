a5d05335ecdfd5a301a67ea2e9a7eb47366ab6d7
Arya <bartararya@gmail.com>
2020-02-12 11:14:45 +0330

    Chage CustomUserAdmin

--- code/sNeeds/apps/customAuth/admin.py
+++ code/sNeeds/apps/customAuth/admin.py
@@ -15,7 +15,7 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+        (_('Permissions'), {'fields': ('user_type', 'is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
