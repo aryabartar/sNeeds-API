f9b72636c079a7554cb145d9f13bbf82bbb3fa9a
Arya <bartararya@gmail.com>
2020-02-25 18:58:02 +0330

    Update TimeSlotSaleFilter

--- code/sNeeds/apps/store/filtersets.py
+++ code/sNeeds/apps/store/filtersets.py
@@ -7,6 +7,16 @@ class TimeSlotSaleFilter(filters.FilterSet):
     min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
     max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
 
+    min_start_time = filters.DateTimeFilter(field_name="start_time", lookup_expr="gte")
+    max_start_time = filters.DateTimeFilter(field_name="start_time", lookup_expr="lte")
+
+    min_end_time = filters.DateTimeFilter(field_name="end_time", lookup_expr="gte")
+    max_end_time = filters.DateTimeFilter(field_name="end_time", lookup_expr="lte")
+
     class Meta:
         model = models.TimeSlotSale
-        fields = ['consultant', 'start_time', 'end_time', 'min_price', 'max_price', 'price']
+        fields = [
+            'consultant', 'min_start_time', 'max_start_time',
+            'min_end_time', 'max_end_time',
+            'end_time', 'min_price', 'max_price', 'price'
+        ]
