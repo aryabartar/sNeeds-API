2198193a3665330d939da5f116e798df62add938
Mohammadreza ghofrani <mohammadrezaghofrany@gmail.com>
2020-02-25 15:31:28 +0330

    General structure of feature that admin could see chats implemented

--- src/sNeeds/settings/settings.py
+++ src/sNeeds/settings/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, "..", "templates")
 
 # Imported key to prevent circular imports.
 from .secure import keys
@@ -70,7 +71,7 @@ ROOT_URLCONF = 'sNeeds.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
