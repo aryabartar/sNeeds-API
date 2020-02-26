92c60cb1c78453db58771f47311376cc56fe608f
Mohammadreza ghofrani <mohammadrezaghofrany@gmail.com>
2020-02-26 19:31:42 +0330

    Added some query parameter to filter, going to beautify messages

--- src/sNeeds/apps/chats/views.py
+++ src/sNeeds/apps/chats/views.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views import generic, View
+from django.shortcuts import render
 
 from rest_framework import status, generics, mixins, permissions
 from rest_framework.response import Response
@@ -120,3 +121,66 @@ class AdminChatView(UserPassesTestMixin, View):
             if self.request.user.is_superuser:
                 return True
         return False
+
+
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+
+def filter(request, id):
+    qs = Message.objects.filter(chat_id=id)
+    text_message_search_character_query = request.GET.get('text_message_search_character')
+    send_date = request.GET.get('send_date')
+    # title_or_author_query = request.GET.get('title_or_author')
+    # view_count_min = request.GET.get('view_count_min')
+    # view_count_max = request.GET.get('view_count_max')
+    # date_min = request.GET.get('date_min')
+    # date_max = request.GET.get('date_max')
+    # category = request.GET.get('category')
+    # reviewed = request.GET.get('reviewed')
+    # not_reviewed = request.GET.get('notReviewed')
+
+    if is_valid_queryparam(text_message_search_character_query):
+        qs = qs.filter(textmessage__text_message__icontains=text_message_search_character_query)
+    if is_valid_queryparam(send_date):
+        qs = qs.filter(created__gte=send_date)
+    #
+    # elif is_valid_queryparam(id_exact_query):
+    #     qs = qs.filter(id=id_exact_query)
+    #
+    # elif is_valid_queryparam(title_or_author_query):
+    #     qs = qs.filter(Q(title__icontains=title_or_author_query)
+    #                    | Q(author__name__icontains=title_or_author_query)
+    #                    ).distinct()
+    #
+    # if is_valid_queryparam(view_count_min):
+    #     qs = qs.filter(views__gte=view_count_min)
+    #
+    # if is_valid_queryparam(view_count_max):
+    #     qs = qs.filter(views__lt=view_count_max)
+    #
+    # if is_valid_queryparam(date_min):
+    #     qs = qs.filter(publish_date__gte=date_min)
+    #
+    # if is_valid_queryparam(date_max):
+    #     qs = qs.filter(publish_date__lt=date_max)
+    #
+    # if is_valid_queryparam(category) and category != 'Choose...':
+    #     qs = qs.filter(categories__name=category)
+
+    # if reviewed == 'on':
+    #     qs = qs.filter(reviewed=True)
+    #
+    # elif not_reviewed == 'on':
+    #     qs = qs.filter(reviewed=False)
+
+    return qs
+
+
+def admin_chat_peek(request, id):
+    qs = filter(request, id)
+    context = {
+        'queryset': qs,
+    }
+    return render(request, "chats/admin_chat_detail.html", context)
+
