d017055b57ec6c09eb39ac5dcee941bf2525db06
Arya <bartararya@gmail.com>
2020-02-28 12:07:49 +0330

    Fix payment bug

--- code/sNeeds/apps/payments/urls.py
+++ code/sNeeds/apps/payments/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "payment"
 
 urlpatterns = [
-    path('request/', views.SendRequest.as_view(), name='request'),
+    path('request/<int:cartid>', views.SendRequest.as_view(), name='request'),
     path('verify/', views.Verify.as_view(), name='verify'),
     path('verify-test/', views.VerifyTest.as_view(), name='verify-test'),
 ]
--- code/sNeeds/apps/payments/views.py
+++ code/sNeeds/apps/payments/views.py
@@ -27,26 +27,31 @@ class SendRequest(APIView):
         user = request.user
 
         try:
-            order = Order.objects.get(cart__user=user)
-        except Order.DoesNotExist:
-            return Response({"detail": "User has no order"}, 400)
+            cart_id = kwargs.get("cartid")
+            cart = Cart.objects.get(id=cart_id)
+        except Cart.DoesNotExist:
+            return Response({"detail": "No cart exists."}, 400)
 
-        if not order.is_acceptable_for_pay():
-            return Response({"detail": "Can not pay this order"}, 400)
+        if not cart.user == user:
+            return Response({"detail": "This user is not cart's owner."}, 400)
+
+        # TODO: Change for 100% discounts.
+        if not cart.is_acceptable_for_pay():
+            return Response({"detail": "Can not pay, The price is 0."}, 400)
 
         result = client.service.PaymentRequest(
             ZARINPAL_MERCHANT,
-            int(order.total),
+            int(cart.total),
             "پرداخت اسنیدز",
-            order.cart.user.email,
-            order.cart.user.phone_number,
-            "http://195.248.243.68/payment/accept/",
+            cart.user.email,
+            cart.user.phone_number,
+            "http://37.152.182.253/payment/accept/",
         )
 
         if result.Status != 100:
-            return Response({"detail": 'Error code: ' + str(result.Status)}, 200)
+            return Response({"detail": 'Error code: ' + str(result.Status)}, 400)
 
-        PayPayment.objects.create(user=user, order=order, authority=result.Authority)
+        PayPayment.objects.create(user=user, cart=cart, authority=result.Authority)
 
         return Response({"redirect": 'https://www.zarinpal.com/pg/StartPay/' + str(result.Authority)})
 
@@ -58,7 +63,8 @@ class Verify(APIView):
         client = Client('https://www.zarinpal.com/pg/services/WebGate/wsdl')
 
         data = request.data
-        if data.get('status', None) == 'OK':
+        print(data)
+        if data.get('status') == 'OK':
             user = request.user
             authority = data.get('authority', None)
 
@@ -71,10 +77,10 @@ class Verify(APIView):
             except PayPayment.DoesNotExist:
                 return Response({"detail": "PayPayment does not exists."}, status=400)
 
-            result = client.service.PaymentVerification(ZARINPAL_MERCHANT, authority, int(payment.order.total))
+            result = client.service.PaymentVerification(ZARINPAL_MERCHANT, authority, int(payment.cart.total))
 
             if result.Status == 100:
-                # Order.objects.sell_order(payment.cart)
+                Order.objects.sell_cart_create_order(payment.cart)
                 return Response({"detail": "Success", "ReflD": str(result.RefID)}, status=200)
             elif result.Status == 101:
                 return Response({"detail": "Transaction submitted", "status": str(result.Status)}, status=200)
