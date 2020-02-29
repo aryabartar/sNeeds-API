5d61d4f8255c953a2d905d7c8cdfc61a9b0968ce
Arya <bartararya@gmail.com>
2020-02-29 08:38:41 +0330

    Update CommentListView

--- code/sNeeds/apps/comments/views.py
+++ code/sNeeds/apps/comments/views.py
@@ -4,14 +4,14 @@ from rest_framework.views import APIView
 
 from .models import ConsultantComment, SoldTimeSlotRate
 from .serializers import CommentSerializer, SoldTimeSlotRateSerializer
-from .permissions import CommentOwnerPermission, SoldTimeSlotRateOwnerPermission
+from .permissions import SoldTimeSlotRateOwnerPermission
 from .filtersets import CommentFilterSet
 
 
 class CommentListView(generics.ListCreateAPIView):
     queryset = ConsultantComment.objects.all().order_by('-created')
     serializer_class = CommentSerializer
-    permission_classes = [CommentOwnerPermission, permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     filterset_class = CommentFilterSet
 
 
@@ -19,7 +19,6 @@ class CommentDetailView(generics.RetrieveAPIView):
     lookup_field = 'id'
     queryset = ConsultantComment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class SoldTimeSlotRateListView(generics.ListCreateAPIView):
