52227c37b9560935e9cda68363ca5b4cc7027c83
Mohammadreza ghofrani <mohammadrezaghofrany@gmail.com>
2020-03-04 14:33:42 +0330

    Message Deleted from admin view

--- code/sNeeds/apps/chats/admin.py
+++ code/sNeeds/apps/chats/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Chat)
-admin.site.register(models.Message)
 admin.site.register(models.TextMessage)
 admin.site.register(models.FileMessage)
 admin.site.register(models.VoiceMessage)
