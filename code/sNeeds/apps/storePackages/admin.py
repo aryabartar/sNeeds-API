b73f876f30a428f5a11129834f19216c45667c51
Arya <bartararya@gmail.com>
2020-03-16 12:53:41 +0330

    Update storepackage admin

--- code/sNeeds/apps/storePackages/admin.py
+++ code/sNeeds/apps/storePackages/admin.py
@@ -3,15 +3,17 @@ from django.contrib import admin
 from .models import (StorePackage, StorePackagePhase, SoldStorePackage, StorePackagePhaseThrough)
 
 
-class StorePackageDetailPhaseThroughInline(admin.TabularInline):
+class StorePackagePhaseThroughInline(admin.TabularInline):
     model = StorePackagePhaseThrough
     extra = 1
 
 
-class StorePackageDetailAdmin(admin.ModelAdmin):
-    inlines = (StorePackageDetailPhaseThroughInline,)
+@admin.register(StorePackage)
+class StorePackageAdmin(admin.ModelAdmin):
+    inlines = (StorePackagePhaseThroughInline,)
+    readonly_fields = ["price", ]
 
 
 # admin.site.register(SoldStorePackage)
 admin.site.register(StorePackagePhase)
-admin.site.register(StorePackage, StorePackageDetailAdmin)
+# admin.site.register(StorePackage, StorePackageAdmin)
