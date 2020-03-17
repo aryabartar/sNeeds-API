00253c0fa73b243071e3fb5c207420e14a0e5783
Arya <bartararya@gmail.com>
2020-03-17 14:33:52 +0330

    Test

--- code/sNeeds/settings/middlewares/middlewares.py
+++ code/sNeeds/settings/middlewares/middlewares.py
@@ -22,9 +22,13 @@ class CORSMiddleware(object):
 
     def __call__(self, request):
         import json
+        print("request: ")
         print(json.dumps(dict(request.headers), indent=4, sort_keys=True))
+        print("------------------------ ")
 
         response = self.get_response(request)
         response["Access-Control-Allow-Origin"] = "*"
 
+        print("response: ")
+        print(json.dumps(dict(response.headers), indent=4, sort_keys=True))
         return response
