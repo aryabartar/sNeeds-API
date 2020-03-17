b80f3ef6a87e11ae7e597742ac903f22571d4d1c
Arya <bartararya@gmail.com>
2020-03-17 14:57:56 +0330

    Test

--- code/test.py
+++ code/test.py
@@ -4,6 +4,8 @@ import requests
 
 headers = {'content-type': 'application/json', 'Origin': 'https://foo.example'}
 
-r = requests.get('http://127.0.0.1:8000/', headers=headers)
+r = requests.get('http://37.152.182.253:8000/', headers=headers)
 print(r.headers)
+print("-----")
+print(r)
 # print(json.dumps(r.headers, indent=3))
