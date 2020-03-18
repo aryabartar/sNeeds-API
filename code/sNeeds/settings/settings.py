ec4935a20b762420df1a0473a745a6078267483c
Arya <bartararya@gmail.com>
2020-03-18 17:48:43 +0300

    Update gitignore

--- code/sNeeds/settings/settings.py
+++ code/sNeeds/settings/settings.py
@@ -109,6 +109,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "..", "files"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/files/'
 MEDIA_ROOT = 'files'
