31819102b52f7be59a0b2b39c017013d897812aa
Arya <bartararya@gmail.com>
2020-03-25 23:36:38 +0430

    Fix my-account bug

--- code/test.py
+++ code/test.py
@@ -4,11 +4,10 @@ import requests
 
 headers = {
     'content-type': 'application/json',
-    'authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImJhcnRhcmFyeWFAZ21haWwuY29tIiwiZXhwIjoxNTg1NzYzMjc4LCJlbWFpbCI6ImJhcnRhcmFyeWFAZ21haWwuY29tIiwib3JpZ19pYXQiOjE1ODUxNTg0Nzh9.7bQaiOQ9KY6_byO4aNF9kPL8YE7-V-TXeOttWVNCX-M',
     'akbar': 'ali',
 }
 
-r = requests.get('http://194.5.206.177:8000/auth/my-account/', headers=headers)
+r = requests.options('http://127.0.0.1:8000/auth/my-account/', headers=headers)
 print(r.headers)
 print(r.text)
 print(r.content)
