f0266eaaa84d1b899ef852beb9cab3a044c88d70
Arya <bartararya@gmail.com>
2020-03-25 22:50:04 +0430

    Test

--- code/test.py
+++ code/test.py
@@ -4,30 +4,11 @@ import requests
 
 headers = {
     'content-type': 'application/json',
-    'authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo1LCJ1c2VybmFtZSI6ImEua2hhbGlnaEBhdXQuYWMuaXIiLCJleHAiOjE1ODU3NjIwNDgsImVtYWlsIjoiYS5raGFsaWdoQGF1dC5hYy5pciIsIm9yaWdfaWF0IjoxNTg1MTU3MjQ4fQ.vA-7nx5BQZw4YkTkQUJlYQTKhN3vdC0fbfgep-yphmM',
-    'akbar':'ali',
+    'authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImJhcnRhcmFyeWFAZ21haWwuY29tIiwiZXhwIjoxNTg1NzYzMjc4LCJlbWFpbCI6ImJhcnRhcmFyeWFAZ21haWwuY29tIiwib3JpZ19pYXQiOjE1ODUxNTg0Nzh9.7bQaiOQ9KY6_byO4aNF9kPL8YE7-V-TXeOttWVNCX-M',
+    'akbar': 'ali',
 }
 
-r = requests.get('http://37.152.182.253:8000/auth/my-account/', headers=headers)
+r = requests.get('http://194.5.206.177:8000/auth/my-account/', headers=headers)
 print(r.headers)
 print(r.text)
 print(r.content)
-
-# Accept: application/json, text/plain, */*
-# Accept-Encoding: gzip, deflate
-# Accept-Language: en-US,en;q=0.9,fa;q=0.8
-# Cache-Control: no-cache
-# Host: 194.5.206.177:8000
-# Origin: http://194.5.206.177
-# Pragma: no-cache
-# Proxy-Authorization: Basic LnN2QDIzODgwMDk7aXIuOlNPM2JNSU9STE0raHg2YnBLWmdkbGNTeUM2ZUhsQXh5T1pFdXNjZlFrZVU9
-# Proxy-Connection: keep-alive
-# Referer: http://194.5.206.177/consultants
-# User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36
-#
-#
-# Accept: application/json, text/plain, */*
-# Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo1LCJ1c2VybmFtZSI6ImEua2hhbGlnaEBhdXQuYWMuaXIiLCJleHAiOjE1ODU3NjIwNDgsImVtYWlsIjoiYS5raGFsaWdoQGF1dC5hYy5pciIsIm9yaWdfaWF0IjoxNTg1MTU3MjQ4fQ.vA-7nx5BQZw4YkTkQUJlYQTKhN3vdC0fbfgep-yphmM
-# CLIENT-TIMEZONE: Asia/Tehran
-# Referer: http://37.152.182.253/user/profile
-# User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36
