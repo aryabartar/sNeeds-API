11ef8aca39a30ccaf496349d720ec82bff199961
Arya <bartararya@gmail.com>
2020-04-01 21:09:00 +0430

    Add migration to storePackages

--- code/sNeeds/apps/storePackages/migrations/0001_initial.py
+++ code/sNeeds/apps/storePackages/migrations/0001_initial.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-    initial = True
+    # initial = True
 
     dependencies = [
         ('store', '0007_auto_20200322_1810'),
