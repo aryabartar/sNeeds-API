61c6b51da7bde7d9bc349fb0d4d6a84cb7e785a3
Arya <bartararya@gmail.com>
2020-04-03 13:48:13 +0430

    Add test_sold_store_package_phase_detail_list_get_success

--- code/sNeeds/apps/storePackages/tests/test_apis.py
+++ code/sNeeds/apps/storePackages/tests/test_apis.py
@@ -507,3 +507,31 @@ class TestAPIStorePackage(CustomAPITestCase):
         response = client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 0)
+
+
+    def test_sold_store_package_phase_detail_list_get_success(self):
+        client = self.client
+
+        url = reverse(
+            "store-package:sold-store-package-phase-detail-list"
+        )
+
+        client.login(email='u1@g.com', password='user1234')
+        response = client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+
+        client.login(email='c1@g.com', password='user1234')
+        response = client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+
+        client.login(email='u2@g.com', password='user1234')
+        response = client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
+        client.login(email='c2@g.com', password='user1234')
+        response = client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
