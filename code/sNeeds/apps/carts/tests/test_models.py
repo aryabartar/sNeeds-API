6b0ab29d7f9ba99ad90e3d0517103ed75ab60de3
hossein_7 <hossein.slayer@gmail.com>
2020-04-07 15:34:18 +0430

    write first tests foe carts

--- code/sNeeds/apps/carts/tests/test_models.py
+++ code/sNeeds/apps/carts/tests/test_models.py
@@ -28,4 +28,36 @@ class CartModelTests(CustomAPITestCase):
         with self.assertRaises(ValidationError) as e:
             Cart.objects.create(user=self.consultant1)
 
-    # def test_product_price_update_
+    def test_change_product_to_inactive_remove_from_products_total_subtotal_correct_without_discount(self):
+        cart = self.cart4
+        products = self.cart4.products.all()
+
+        subtotal = 0
+        for p in products:
+            subtotal += p.price
+
+        total = subtotal
+
+        self.assertEqual(cart.subtotal, subtotal)
+        self.assertEqual(cart.total, total)
+
+        self.store_package_1.active = False
+        self.store_package_1.save()
+
+        self.cart4.refresh_from_db()
+        cart = self.cart4
+
+        products = cart.products.all()
+
+        subtotal2 = 0
+        for p in products:
+            subtotal2 += p.price
+        total2 = subtotal2
+
+        print(total)
+        print(self.store_package_1.price)
+        print(total2)
+
+        self.assertEqual(cart.subtotal, subtotal2)
+        self.assertEqual(cart.total, total2)
+        self.assertEqual(total2, total - self.store_package_1.price)
