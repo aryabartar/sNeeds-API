6a5d6fd44133cc7b27431dfff21e94990dc6555b
Arya Khaligh <bartararya@gmail.com>
2020-04-14 13:35:34 -0500

    Update SoldTimeSlotSaleAdmin

--- code/sNeeds/apps/store/admin.py
+++ code/sNeeds/apps/store/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from .models import TimeSlotSale, SoldTimeSlotSale, Product
 
 admin.site.register(Product)
-admin.site.register(SoldTimeSlotSale)
 
 
 @admin.register(TimeSlotSale)
-class StorePackageAdmin(admin.ModelAdmin):
+class TimeSlotSaleAdmin(admin.ModelAdmin):
+    list_display = ["id", "consultant", "start_time", "end_time", "price"]
     readonly_fields = ["price", "active", ]
 
+
+@admin.register(SoldTimeSlotSale)
+class SoldTimeSlotSaleAdmin(admin.ModelAdmin):
+    list_display = ["id", "consultant", "start_time", "end_time", "sold_to", "price"]
+    readonly_fields = ["price", "active", ]
