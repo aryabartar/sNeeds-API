4554b6977724c05679d4a5b50149d923ddd4c2bc
Arya Khaligh <bartararya@gmail.com>
2020-04-15 02:22:36 -0500

    Add SoldTimeSlotSaleAdmin

--- code/sNeeds/apps/store/admin.py
+++ code/sNeeds/apps/store/admin.py
@@ -13,4 +13,3 @@ class TimeSlotSaleAdmin(admin.ModelAdmin):
 @admin.register(SoldTimeSlotSale)
 class SoldTimeSlotSaleAdmin(admin.ModelAdmin):
     list_display = ["id", "consultant", "start_time", "end_time", "sold_to", "price"]
-    readonly_fields = ["price",  ]
