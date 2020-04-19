9ff9954ccf54464c4ac09d927107e229459215a7
Arya Khaligh <bartararya@gmail.com>
2020-04-19 23:54:05 +0430

    Update frontend address

--- code/sNeeds/settings/config/variables.py
+++ code/sNeeds/settings/config/variables.py
@@ -1,6 +1,6 @@
 import os
 
 TIME_SLOT_SALE_DELETE_TIME = 24  # hours
-FRONTEND_URL = "http://194.5.206.177/"
+FRONTEND_URL = "http://new.sneeds.ir/"
 BACKEND_URL = "http://194.5.206.177:8000/"
 SERVER_NAME = SECRET_KEY = os.environ.get('SERVER_NAME')
