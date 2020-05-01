4cedca56ad16acf22d7770115d1110b74f870339
aryabartar <bartararya@gmail.com>
2020-05-01 21:37:55 +0430

    Update jwt settings

--- code/sNeeds/settings/config/JWTAuthConfig.py
+++ code/sNeeds/settings/config/JWTAuthConfig.py
@@ -27,7 +27,7 @@ JWT_AUTH = {
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
     'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(minutes=1),
     'JWT_AUDIENCE': None,
     'JWT_ISSUER': None,
 
