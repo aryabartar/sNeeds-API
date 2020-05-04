c800d17daa7c3a192da6efcd2c1e0ffaecfcbfe1
aryabartar <bartararya@gmail.com>
2020-05-04 17:14:37 +0430

    Update ConsultantProfileSerializer

--- code/sNeeds/apps/consultants/models.py
+++ code/sNeeds/apps/consultants/models.py
@@ -1,3 +1,5 @@
+import random
+
 from ckeditor.fields import RichTextField
 
 from django.db import models
@@ -60,6 +62,8 @@ class ConsultantProfile(models.Model):
 
         sold_time_slot_rate_qs = SoldTimeSlotRate.objects.filter(sold_time_slot__consultant__id=self.id)
         average_rate = sold_time_slot_rate_qs.get_average_rate_or_none()
+        if average_rate is None:
+            self.rate = random.randint(38, 50) / 10
         self.rate = average_rate
         self.save()
 
