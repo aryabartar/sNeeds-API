5f936d85e574230b271b20c9efad1cebf73bdf7e
aryabartar <bartararya@gmail.com>
2020-05-15 12:57:32 +0430

    Update post_save_user

--- code/sNeeds/apps/chats/signals/handlers.py
+++ code/sNeeds/apps/chats/signals/handlers.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import post_save, post_delete
 from django.contrib.auth import get_user_model
 
-from sNeeds.apps.chats.models import Chat
+from sNeeds.apps.chats.models import Chat, TextMessage
+from sNeeds.apps.consultants.models import ConsultantProfile
 
 User = get_user_model()
 
@@ -9,10 +10,33 @@ User = get_user_model()
 def post_save_user(sender, instance, created, *args, **kwargs):
     if created:
         admin_consultant_user = User.objects.get_admin_consultant_or_none()
-        if admin_consultant_user is not None:
-            chat = Chat.objects.get_or_create(
+        if admin_consultant_user is not None and ConsultantProfile.objects.filter(user=admin_consultant_user).exists():
+            chat = Chat.objects.create(
                 user=instance,
-                consultant=admin_consultant_user
+                consultant=ConsultantProfile.objects.get(user=admin_consultant_user)
+            )
+            TextMessage.objects.create(
+                chat=chat,
+                sender=admin_consultant_user,
+                text_message="\
+                    سلام\
+                    به جمع ۱۲۰۰ کاربر اسنیدز خوش اومدی!\
+                    \
+                    اینجا پیام‌های مهم از طرف اسنیدز برات فرستاده می‌شه.\
+                    \
+                    همین طور تو قسمت چت می‌تونی با مشاوری که باهاش تایم مشاوره رزرو کردی در ارتباط باشی!\
+                    \
+                    هر سوالی هم در مورد استفاده از سایت و خدماتمون داری می‌تونی همینجا بپرسی و ما خیلی سریع بهت جواب می‌دیم. " \
+                )
+
+            TextMessage.objects.create(
+                chat=chat,
+                sender=admin_consultant_user,
+                text_message=" \
+                    شماره تماس ما:‌ 09221496645 \
+                    ایمیل:‌ info@sneeds.ir \
+                    تلگرام پشتیبانی:‌sneeds_admin@ \
+                  "
             )
 
 
