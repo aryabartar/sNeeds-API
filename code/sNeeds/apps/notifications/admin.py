0cd4e0c196811c6ce2e2e07a7c2db3771bca88aa
aryabartar <bartararya@gmail.com>
2020-05-16 17:53:53 +0430

    Update notifivations admin

--- code/sNeeds/apps/notifications/admin.py
+++ code/sNeeds/apps/notifications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Notification, EmailNotification
+
+
+@admin.register(EmailNotification)
+class DiscountAdmin(admin.ModelAdmin):
+    readonly_fields = ['created', 'updated', 'sent',]
