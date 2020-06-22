0174d2f93ab9f951966ac82a3ab93f8fa3e60740
aryabartar <bartararya@gmail.com>
2020-06-22 22:39:20 +0430

    Update REST_FRAMEWORK

--- code/sNeeds/settings/settings.py
+++ code/sNeeds/settings/settings.py
@@ -120,6 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
