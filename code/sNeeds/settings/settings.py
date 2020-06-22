edb4f6d422e7a598fb0c90818f77b769147cfcf4
aryabartar <bartararya@gmail.com>
2020-06-22 22:46:08 +0430

    Update REST_FRAMEWORK

--- code/sNeeds/settings/settings.py
+++ code/sNeeds/settings/settings.py
@@ -121,7 +121,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
         # 'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'sNeeds.utils.custom.authentication_classes.CsrfExemptSessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
