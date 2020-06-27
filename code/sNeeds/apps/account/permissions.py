be70b2d2552585d31c75a0e6efe9fdb3d71cfc5e
hossein_7 <hossein.slayer@gmail.com>
2020-06-27 22:31:12 +0430

    permission for get student form edited

--- code/sNeeds/apps/account/permissions.py
+++ code/sNeeds/apps/account/permissions.py
@@ -26,12 +26,13 @@ class StudentDetailedInfoOwnerOrInteractConsultantPermission(permissions.BasePer
             return True
 
         user = request.user
-        if obj.user == user:
-            return True
-        elif request.method == 'GET' and user.is_consultant():
-            """when user has bought a package, every consultant can see the form
-            and if had not bought package any other user can not see that. in this situation to let consultants that
-            had time slot with user to see the form uncommnet the qs_2 line"""
-            # qs_1 = get_consultants_interact_with_user(user=obj.user).filter(user=user)
-            qs_2 = SoldStorePackage.objects.filter(sold_to=obj.user)
-            return qs_2.exists()
+        if user:
+            if obj.user == user:
+                return True
+            elif request.method == 'GET' and user.is_consultant():
+                """when user has bought a package, every consultant can see the form
+                and if had not bought package any other user can not see that. in this situation to let consultants that
+                had time slot with user to see the form uncommnet the qs_2 line"""
+                # qs_1 = get_consultants_interact_with_user(user=obj.user).filter(user=user)
+                qs_2 = SoldStorePackage.objects.filter(sold_to=obj.user)
+                return qs_2.exists()
